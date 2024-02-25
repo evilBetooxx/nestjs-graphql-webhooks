7e8a7b9935e2aac25a340ecb2bbafce6a3fc980c
evilBetooxx <albertof4in@gmail.com>
2024-02-25 09:56:32 -0600

    ATLAS - WebHook de Prueba Funcionando

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/order')
+  createOrder(@Body() data) {
+    const createdOrder = this.appService.createOrder(data);
+
+    this.httpService
+      .post('https://webhook.site/4f03ab92-e3fe-48d3-9471-b740881291f4', data)
+      .subscribe({
+        complete: () => console.log('Webhook sent'),
+        error: (error) => console.log('Error sending webhook', error),
+      });
+
+    return createdOrder;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { HttpModule } from '@nestjs/axios';  
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
